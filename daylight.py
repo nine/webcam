b79b4132b1fb56b3b0032970da8f07fe4fdc9b9d
Erwin Nindl <nine@wirdorange.org>
2015-01-02 00:24:09 +0100

    added argparse

--- daylight.py
+++ daylight.py
@@ -3,9 +3,19 @@
 
 ## Erwin Nindl, 2014
 
+import argparse
 import ephem
 from datetime import datetime
 import sys
+
+
+# parse arguments
+parser = argparse.ArgumentParser(prog="daylight", description='Estimate the presence of daylight')
+parser.add_argument('--lon', type=float, dest='lon', required=True, help='position longitude')
+parser.add_argument('--lat', type=float, dest='lat', required=True, help='position latitude')
+parser.add_argument('--ele', type=int, dest='ele', required=True, help='elevation')
+options = parser.parse_args()
+
 
 # ephem works with UTC
 utc_time = datetime.utcnow()
@@ -14,9 +24,9 @@ now = ephem.Date( utc_time.strftime("%Y/%m/%d %H:%M:%S") )
 #Make an observer
 fred      = ephem.Observer()
 fred.date = utc_time.strftime("%Y/%m/%d 12:00:00")
-fred.lon  = str(12.41335)
-fred.lat  = str(47.28991)
-fred.elev = 950
+fred.lon  = str(options.lon)
+fred.lat  = str(options.lat)
+fred.elev = options.ele
 
 sunrise = fred.previous_rising( ephem.Sun() ) #Sunrise
 noon    = fred.next_transit( ephem.Sun(), start=sunrise ) #Solar noon
